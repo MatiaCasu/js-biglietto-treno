bb5792d1d3a3b8f304616bd86bb56d4a050d6ad8
Matia <matia.casu@gmail.com>
2021-02-26 16:33:51 +0100

    Condizione Prezzo/Età

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
-// comandi Prova
+// Prompt
 var kmUtente = prompt("Ciao! Quanti Km vuoi percorrere?");
-// var eta = prompt("Bene! Ora digita la tua età!");
-// var kmUtente = 20;
-// var eta = 16;
-// /comandi Prova
+var eta = prompt("Bene! Ora digita la tua età!");
+// /Prompt
 
-// Prova prezzo intero
+// Prezzi
+// Prezzo intero
 var prezzoBase = 0.21 * kmUtente;
 
-document.getElementById("prezzo_base").innerHTML = prezzoBase;
-// /Prova prezzo intero
-
-// PREZZO SCONTATO Under18
+// Prezzo Under18
 var sconto18 = prezzoBase * 20 / 100;
 var prezzoUnder18 = prezzoBase - sconto18;
 
-document.getElementById("prezzo_under18").innerHTML = prezzoUnder18;
-// /PREZZO SCONTATO Under18
-
+// Prezzo Over65
 var sconto65 = prezzoBase * 40 / 100;
 var prezzoOver65 = prezzoBase - sconto65;
+// Prezzi
 
-document.getElementById("prezzo_over65").innerHTML = prezzoOver65;
-// /PREZZO SCONTATO Under18
+// Prezzo UTENTE
+if (eta < 18) {
+  var prezzoUtente = prezzoUnder18;
+}
+else if(eta > 65){
+  prezzoUtente = prezzoOver65;
+}
+else{
+  prezzoUtente = prezzoBase;
+}
+
+document.getElementById("prezzo").innerHTML = prezzoUtente;
+// /Prezzo UTENTE
